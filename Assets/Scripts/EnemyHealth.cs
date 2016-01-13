320ea39db1acb8914a14ff3d330fd1dfcd737f85
dendimaniac <daq30899@hotmail.com>
2016-01-13 15:53:44 -0800

    Basic controls (not attack) and animations + 1 enemy

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyHealth : MonoBehaviour {
+
+    public float deadWaitTime;
+    public int startingHealth;
+
+    Collider2D enemyCollider;
+    int currentHealth;
+    bool isDead;
+
+    // Use this for initialization
+    void Awake()
+    {
+        enemyCollider = GetComponent<Collider2D>();
+        currentHealth = startingHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if (isDead)
+            return;
+        currentHealth -= damage;
+        Debug.Log(currentHealth);
+
+        if (currentHealth <= 0)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+        isDead = true;
+        enemyCollider.enabled = false;
+        Destroy(gameObject, deadWaitTime);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed;
+
+    Rigidbody2D playerRigidbody;
+    Vector2 movement;
+    Vector2 direction;
+    Vector2 mousePosition;
+    float h;
+    float v;
+    float angle;
+
+	// Use this for initialization
+	void Awake () {
+        playerRigidbody = GetComponent<Rigidbody2D>();
+	}
+	
+    void Update ()
+    {
+        
+    }
+
+	// Update is called once per frame
+	void FixedUpdate () {
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+
+        Move(h, v);
+        Rotate();
+    }
+
+    void Move (float h, float v)
+    {
+        movement.Set(h,v);
+        movement = movement.normalized * speed * Time.deltaTime;
+        playerRigidbody.velocity = movement;
+    }
+
+    void Rotate ()
+    {
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        direction = (mousePosition - (Vector2)transform.position).normalized;
+        angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+}
